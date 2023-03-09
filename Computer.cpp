4a4acc1422e8c86816bac13d4a945c82e7ca7e38
xioFelix <xiofelix@163.com>
2023-03-09 16:54:26 +1030

    Init

---
+++ Computer.cpp
@@ -0,0 +1,9 @@
+#include "Computer.h"
+
+char Computer::makeMove() {
+    return 'R';
+}
+
+std::string Computer::getName() {
+    return "Computer";
+}
---
+++ Human.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Human.h"
+
+Human::Human(std::string name) : name_Get(name) {}
+
+char Human::makeMove() {
+    std::cout << "Enter move: ";
+    char move;
+    std::cin >> move;
+    return move;
+}
+
+std::string Human::getName() {
+    return name_Get;
+}
