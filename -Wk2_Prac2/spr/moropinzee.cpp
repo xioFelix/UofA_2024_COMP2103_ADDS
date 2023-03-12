fddc60920ee8ff6a7d8044a2cf33527795fa119c
xioFelix <xiofelix@163.com>
2023-03-12 04:59:51 +0000

    Invalid DateTime

--- -Wk2_Prac2/spr/moropinzee.cpp
+++ -Wk2_Prac2/spr/moropinzee.cpp
@@ -16,15 +16,15 @@ moropinzee::moropinzee(std::string _name) {
   if (_name == "Robot") {
     id = 1;
   } else if (_name == "Pirate") {
-    id = 2;
+    id = 1.5;
   } else if (_name == "Ninja") {
-    id = 3;
+    id = 2;
   } else if (_name == "Monkey") {
-    id = 4;
+    id = 2.5;
   } else if (_name == "Zombie") {
-    id = 5;
+    id = 3;
   } else {
-    id = 100;
+    id = 10;
   }
 }
 void moropinzee::setMove(std::string change_) {
@@ -32,15 +32,15 @@ void moropinzee::setMove(std::string change_) {
   if (change_ == "Robot") {
     id = 1;
   } else if (change_ == "Pirate") {
-    id = 2;
+    id = 1.5;
   } else if (change_ == "Ninja") {
-    id = 3;
+    id = 2;
   } else if (change_ == "Monkey") {
-    id = 4;
+    id = 2.5;
   } else if (change_ == "Zombie") {
-    id = 5;
+    id = 3;
   } else {
-    id = 100;
+    id = 10;
   }
 }
 
@@ -51,15 +51,15 @@ int moropinzee::getid() { return id; }
 Move* moropinzee::WinOrLoss(Move* compare) {
   string temp = NameOfMove;
   if (id > compare->getid()) {
-    if ((id - compare->getid()) % 2 != 0) {
+    if ((id - compare->getid()) % 1 != 0) {
       return new moropinzee(temp);
-    } else if ((id - compare->getid()) % 2 == 0) {
+    } else if ((id - compare->getid()) % 1 == 0) {
       return compare;
     }
   } else if (compare->getid() > id) {
-    if ((compare->getid() - id) % 2 != 0) {
+    if ((compare->getid() - id) % 1 != 0) {
       return compare;
-    } else if ((compare->getid() - id) % 2 == 0) {
+    } else if ((compare->getid() - id) % 1 == 0) {
       return new moropinzee(temp);
     }
   } else if (id == compare->getid()) {
