b10185df95ca8343d9bafb0936ddce2ae838f8b7
xioFelix <xiofelix@163.com>
2023-03-12 07:59:11 +0000

    Invalid DateTime

--- -Wk2_Prac2/spr/moropinzee.cpp
+++ -Wk2_Prac2/spr/moropinzee.cpp
@@ -12,34 +12,34 @@ moropinzee::moropinzee() {}
 string moropinzee::getName() { return NameOfMove; }
 float moropinzee::getid() { return id; }
 
-moropinzee::moropinzee(std::string _name) {
-  NameOfMove = _name;
-  if (_name == "Robot") {
+moropinzee::moropinzee(std::string moveName) {
+  NameOfMove = moveName;
+  if (moveName == "Robot") {
     id = 1;
-  } else if (_name == "Pirate") {
+  } else if (moveName == "Pirate") {
     id = 1.5;
-  } else if (_name == "Ninja") {
+  } else if (moveName == "Ninja") {
     id = 2;
-  } else if (_name == "Monkey") {
+  } else if (moveName == "Monkey") {
     id = 2.5;
-  } else if (_name == "Zombie") {
+  } else if (moveName == "Zombie") {
     id = 3;
   } else {
     id = 10;
   }
 }
 
-void moropinzee::setMove(std::string change_) {
-  NameOfMove = change_;
-  if (change_ == "Robot") {
+void moropinzee::setMove(std::string RPMoveName) {
+  NameOfMove = RPMoveName;
+  if (RPMoveName == "Robot") {
     id = 1;
-  } else if (change_ == "Pirate") {
+  } else if (RPMoveName == "Pirate") {
     id = 1.5;
-  } else if (change_ == "Ninja") {
+  } else if (RPMoveName == "Ninja") {
     id = 2;
-  } else if (change_ == "Monkey") {
+  } else if (RPMoveName == "Monkey") {
     id = 2.5;
-  } else if (change_ == "Zombie") {
+  } else if (RPMoveName == "Zombie") {
     id = 3;
   } else {
     id = 10;
