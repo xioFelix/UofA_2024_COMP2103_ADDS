d7f5a0ce0a0619332b2fbc1059fa89a99272a47c
xioFelix <xiofelix@163.com>
2023-03-13 00:00:46 +0000

    Mar 13, 2023, 10:30 AM

--- Referee.cpp
+++ Referee.cpp
@@ -8,6 +8,27 @@
 #include "rps.h"
 Referee::Referee() {}
 
+// Player* Referee::refGame(Player* player1, Player* player2) {
+//   Move* m1 = new Move;
+//   Move* m2 = new Move;
+//   m1 = player1->makeMove();
+//   m2 = player2->makeMove();
+//   Move* winner = new Move;
+//   winner = m1->Judge(m2);
+//   if (winner == nullptr) {
+//     // delete m1, m2, winner;
+//     return nullptr;
+//   } else if (winner->getName() == m2->getName()) {
+//     // delete m1, m2, winner;
+//     return player2;
+//   } else if (winner->getName() == m1->getName()) {
+//     // delete m1, m2, winner;
+//     return player1;
+//   }
+
+//   return nullptr;
+// }
+
 Player* Referee::refGame(Player* player1, Player* player2) {
   Move* m1 = new Move;
   Move* m2 = new Move;
@@ -15,16 +36,12 @@ Player* Referee::refGame(Player* player1, Player* player2) {
   m2 = player2->makeMove();
   Move* winner = new Move;
   winner = m1->Judge(m2);
-  if (winner == nullptr) {
-    // delete m1, m2, winner;
-    return nullptr;
-  } else if (winner->getName() == m2->getName()) {
+  if (winner->getName() == m2->getName()) {
     // delete m1, m2, winner;
     return player2;
   } else if (winner->getName() == m1->getName()) {
     // delete m1, m2, winner;
     return player1;
   }
-
   return nullptr;
 }
