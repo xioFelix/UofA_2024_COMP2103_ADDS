2e7dda0e305073615f122d994bbc1fbec0eba98c
xioFelix <xiofelix@163.com>
2023-03-13 06:03:29 +0000

    Mar 13, 2023, 4:33 PM

--- Referee.cpp
+++ Referee.cpp
@@ -9,19 +9,20 @@
 Referee::Referee() {}
 
 Player* Referee::refGame(Player* player1, Player* player2) {
-  Move* m1 = player1->makeMove();
-  Move* m2 = player2->makeMove();
-  Move* winner = m1->Judge(m2);
-  
-  // Deallocate memory
-  delete m1;
-  delete m2;
-  
+  Move* m1 = new Move;
+  Move* m2 = new Move;
+  m1 = player1->makeMove();
+  m2 = player2->makeMove();
+  Move* winner = new Move;
+  winner = m1->Judge(m2);
   if (winner == nullptr) {
+    // delete m1, m2, winner;
     return nullptr;
   } else if (winner->getName() == m2->getName()) {
+    // delete m1, m2, winner;
     return player2;
   } else if (winner->getName() == m1->getName()) {
+    // delete m1, m2, winner;
     return player1;
   }
 
