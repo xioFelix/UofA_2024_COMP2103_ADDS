33b142f894003c9c6e40d38ee8a4353fef124ded
xioFelix <xiofelix@163.com>
2023-03-13 00:08:08 +0000

    Mar 13, 2023, 10:38 AM

--- Referee.cpp
+++ Referee.cpp
@@ -15,13 +15,16 @@ Player* Referee::refGame(Player* player1, Player* player2) {
   m2 = player2->makeMove();
   Move* winner = new Move;
   winner = m1->Judge(m2);
-  if (winner->getName() == m2->getName()) {
+  if (winner == nullptr) {
+    // delete m1, m2, winner;
+    return nullptr;
+  } else if (winner->getName() == m2->getName()) {
     // delete m1, m2, winner;
     return player2;
-
   } else if (winner->getName() == m1->getName()) {
     // delete m1, m2, winner;
     return player1;
   }
+
   return nullptr;
 }
