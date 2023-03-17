728524b8d04cf2b4e2d18d2be9af2e15ca1f014e
xioFelix <xiofelix@163.com>
2023-03-17 23:32:53 +1030

    2

---
+++ Reverser.h
@@ -0,0 +1,14 @@
+#ifndef REVERSER_H
+#define REVERSER_H
+#include <math.h>
+
+#include <string>
+
+using namespace std;
+
+class Reverse {
+ public:
+  int reverseDigit(int value);
+  string reverseString(string letters);
+};
+#endif
