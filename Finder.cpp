8dd4682ce80aeb82bc299ed253b055a8522734da
xioFelix <xiofelix@163.com>
2023-04-01 16:46:55 +0000

    Invalid DateTime

--- Finder.cpp
+++ Finder.cpp
@@ -1,20 +1,41 @@
+// #include "Finder.h"
+
+//     using namespace std;
+
+//     vector<int> Finder::findSubstrings(string s1, string s2) {
+
+//         vector<int> result;
+
+//         for(size_t i = 1; i <= s2.size(); i++) {
+//             size_t found = s1.find(s2.substr(0, i));
+//             if (found != string::npos) {
+//                 result.push_back(found);
+//             } else {
+//                 result.push_back(-1);
+//             }
+//         }
+//         return result;
+//     }
+
 #include "Finder.h"
 
-    using namespace std;
+using namespace std;
 
-    vector<int> Finder::findSubstrings(string s1, string s2) {
+vector<int> Finder::findSubstrings(string s1, string s2) {
+  vector<int> result;
+  result.reserve(s2.size());
 
-        vector<int> result;
-
-        for(size_t i = 1; i <= s2.size(); i++) {
-            size_t found = s1.find(s2.substr(0, i));
-            if (found != string::npos) {
-                result.push_back(found);
-            } else {
-                result.push_back(-1);
-            }
-        }
-        return result;
+  string substr = s2.substr(0, 1);
+  for (size_t i = 1; i <= s2.size(); i++) {
+    size_t found = s1.find(substr);
+    if (found != string::npos) {
+      result.push_back(found);
+    } else {
+      result.push_back(-1);
     }
-
-   
+    if (i < s2.size()) {
+      substr.replace(0, 1, 1, s2[i]);
+    }
+  }
+  return result;
+}
