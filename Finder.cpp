ea83ae613f31672d5efe6ef2991aefd6fa901530
xioFelix <xiofelix@163.com>
2023-04-02 02:50:26 +1030

    init

---
+++ Finder.cpp
@@ -0,0 +1,20 @@
+#include "Finder.h"
+
+    using namespace std;
+
+    vector<int> Finder::findSubstrings(string s1, string s2) {
+
+        vector<int> result;
+
+        for(size_t i = 1; i <= s2.size(); i++) {
+            size_t found = s1.find(s2.substr(0, i));
+            if (found != string::npos) {
+                result.push_back(found);
+            } else {
+                result.push_back(-1);
+            }
+        }
+        return result;
+    }
+
+   
