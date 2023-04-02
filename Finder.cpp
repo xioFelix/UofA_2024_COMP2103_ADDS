8821618ab6fa24dafd2b5123f4c468048334127e
xioFelix <xiofelix@163.com>
2023-04-02 05:36:50 +0000

    Invalid DateTime

--- Finder.cpp
+++ Finder.cpp
@@ -5,13 +5,13 @@ using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
   vector<int> result;
   string substr = s2.substr(0, 1);
-  size_t index=0;
+  size_t index = 0;
 
   for (size_t i = 1; i <= s2.size(); i++) {
     size_t found = s1.find(s2.substr(0, i), index);
     if (found != string::npos) {
       result.push_back(found);
-      index=found+1;
+      index=found;
     } else {
       result.push_back(-1);
       break;
