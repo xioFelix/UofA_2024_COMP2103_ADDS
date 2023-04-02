184829e7d083f6c3d6b394c588344ec28bca2199
xioFelix <xiofelix@163.com>
2023-04-02 03:51:52 +0000

    Invalid DateTime

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Finder.cpp"
+using namespace std;
+
+int main() {
+  Finder finder;
+
+  string s1 = "hello world";
+  string s2 = "he";
+
+  vector<int> result = finder.findSubstrings(s1, s2);
+
+  cout << "s1: " << s1 << endl;
+  cout << "s2: " << s2 << endl;
+  cout << "Result: ";
+  for (int i = 0; i < result.size(); i++) {
+    cout << result[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
