92fa39a88897a4e3bd40732cd2035dfc70225f90
xioFelix <xiofelix@163.com>
2023-04-02 04:17:15 +0000

    Invalid DateTime

--- Finder.cpp
+++ Finder.cpp
@@ -23,8 +23,6 @@ using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
   vector<int> result;
-  result.reserve(s2.size());
-
   string substr = s2.substr(0, 1);
   for (size_t i = 1; i <= s2.size(); i++) {
     size_t found = s1.find(substr);
--- main.cpp
+++ main.cpp
@@ -1,34 +1,16 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <chrono>
+#include "Finder.h"
 #include "Finder.cpp"
-
 using namespace std;
-using namespace chrono;
 
 int main() {
   Finder finder;
-  string s1, s2;
-  vector<int> result;
-  ifstream infile("data.txt");
 
-  // Read data from file
-  if (infile) {
-    getline(infile, s1);
-    getline(infile, s2);
-  } else {
-    cerr << "Error: failed to open input file" << endl;
-    return -1;
-  }
+  string s1 = "hello world";
+  string s2 = "he";
 
-  // Measure execution time of findSubstrings function
-  auto start = high_resolution_clock::now();
-  result = finder.findSubstrings(s1, s2);
-  auto stop = high_resolution_clock::now();
-  auto duration = duration_cast<microseconds>(stop - start);
+  vector<int> result = finder.findSubstrings(s1, s2);
 
-  // Print results
   cout << "s1: " << s1 << endl;
   cout << "s2: " << s2 << endl;
   cout << "Result: ";
@@ -36,7 +18,6 @@ int main() {
     cout << result[i] << " ";
   }
   cout << endl;
-  cout << "Execution time: " << duration.count() << " microseconds" << endl;
 
   return 0;
 }
