7bd0cdc71b09620711cb3cd89999dcf6ccddae0d
xioFelix <xiofelix@163.com>
2023-04-02 03:54:58 +0000

    Invalid DateTime

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
 #include "Finder.cpp"
+
 using namespace std;
+using namespace chrono;
 
 int main() {
   Finder finder;
+  string s1, s2;
+  vector<int> result;
+  ifstream infile("data.txt");
 
-  string s1 = "hello world";
-  string s2 = "he";
+  // Read data from file
+  if (infile) {
+    getline(infile, s1);
+    getline(infile, s2);
+  } else {
+    cerr << "Error: failed to open input file" << endl;
+    return -1;
+  }
 
-  vector<int> result = finder.findSubstrings(s1, s2);
+  // Measure execution time of findSubstrings function
+  auto start = high_resolution_clock::now();
+  result = finder.findSubstrings(s1, s2);
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
 
+  // Print results
   cout << "s1: " << s1 << endl;
   cout << "s2: " << s2 << endl;
   cout << "Result: ";
@@ -17,6 +36,7 @@ int main() {
     cout << result[i] << " ";
   }
   cout << endl;
+  cout << "Execution time: " << duration.count() << " microseconds" << endl;
 
   return 0;
 }
