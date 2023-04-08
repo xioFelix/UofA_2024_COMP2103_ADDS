24fd412137743e919e092abd39d92fe3139c9b0b
Felix <a1878510@adelaide.edu.au>
2023-04-08 00:36:32 +0000

    2023年4月8日 10:06

--- ADDS-practical7/BubbleSort.cpp
+++ ADDS-practical7/BubbleSort.cpp
@@ -1,36 +1,25 @@
 #include "BubbleSort.h"
+
 #include <vector>
 
 using namespace std;
 
-BubbleSort::BubbleSort()
-{
-}
+BubbleSort::BubbleSort() {}
 
-
-void BubbleSort::sort(vector<int> &list)
-{
-    bool wasSwap = 0;
-    for (int i = 0; i < list.size(); i++)
-    {
-        for (int j = 0; j < list.size()-1; j++)
-        {
-            if(list.at(j) > list.at(j+1))
-            {
-                swap(list.at(j), list.at(j+1));
-                wasSwap = 1;
-            }
-        }
-
-        if( !wasSwap )
-        {
-            break;
-        }
+void BubbleSort::sort(vector<int> &list) {
+  bool wasSwap = 0;
+  for (int i = 0; i < list.size(); i++) {
+    for (int j = 0; j < list.size() - 1; j++) {
+      if (list.at(j) > list.at(j + 1)) {
+        swap(list.at(j), list.at(j + 1));
+        wasSwap = 1;
+      }
     }
+
+    if (!wasSwap) {
+      break;
+    }
+  }
 }
 
-
-BubbleSort::~BubbleSort()
-{
-
-}
+BubbleSort::~BubbleSort() {}
