7f5930e07f4aefa1804b86e491f114e14836ad33
Felix <a1878510@adelaide.edu.au>
2023-04-10 04:59:26 +0000

    2023年4月10日 14:29

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "BubbleSort.h"
+#include "QuickSort.h"
+#include "RecursiveBinarySearch.h"
+#include "Sort.h"
+using namespace std;
+
+int main() {
+  vector<int> input;
+  string entry;
+  getline(cin, entry);
+  istringstream out(entry);
+  int value;
+  while (out >> value) {
+    input.push_back(value);
+  }
+
+  BubbleSort myb;
+  myb.sort(input);
+  QuickSort myq;
+  myq.sort(input, 0, input.size() - 1);
+
+  RecursiveBinarySearch mybin;
+  int found = mybin.search(input, 0, 0, input.size() - 1);
+  if (found >= 0) {
+    cout << "true";
+  } else {
+    cout << "false";
+  }
+
+  for (unsigned int i = 0; i < input.size(); i++) {
+    cout << " " << input.at(i);
+  }
+}
