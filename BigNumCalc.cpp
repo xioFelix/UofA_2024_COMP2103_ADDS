7578d97b2f2fc58b1d7955fa84a97858234accfd
Felix <a1878510@adelaide.edu.au>
2023-05-02 13:01:35 +0930

    15

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -55,18 +55,28 @@ std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2) {
 }
 
 std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2) {
-  std::list<int> result;
-  int carry = 0;
-  for (int digit : num1) {
-    int prod = digit * num2.front() + carry;
-    carry = prod / 10;
-    result.push_back(prod % 10);
-  }
-  if (carry > 0) {
-    result.push_back(carry);
-  }
-  while (result.size() > 1 && result.front() == 0) {
-    result.pop_front();
+  int factor = num2.back();
+  num2.pop_back();
+  std::list<int> result = add(std::list<int>(), std::list<int>());
+  while (!num1.empty()) {
+    int product = num1.back() * factor;
+    num1.pop_back();
+    std::list<int> partialResult;
+    int carry = 0;
+    while (product || carry) {
+      int digit = carry + product % 10;
+      product /= 10;
+      carry = digit / 10;
+      digit %= 10;
+      partialResult.push_front(digit);
+    }
+    while (partialResult.front() == 0 && partialResult.size() > 1) {
+      partialResult.pop_front();
+    }
+    for (int i = 0; i < num1.size(); i++) {
+      partialResult.push_back(0);
+    }
+    result = add(result, partialResult);
   }
   return result;
 }
