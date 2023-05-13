4c5ef61304d22b3f0bf5778296ae75d4fbda2654
Felix <a1878510@adelaide.edu.au>
2023-05-13 16:17:14 +0000

    2023年5月14日 01:47

---
+++ PrefixMatcher.cpp
@@ -0,0 +1,38 @@
+#include "PrefixMatcher.h"
+
+BinaryTrieNode::BinaryTrieNode() {
+  routerNumber = -1;
+  children[0] = nullptr;
+  children[1] = nullptr;
+}
+
+PrefixMatcher::PrefixMatcher() { root = new BinaryTrieNode(); }
+
+int PrefixMatcher::selectRouter(const std::string& networkAddress) {
+  BinaryTrieNode* current = root;
+  int selectedRouter = -1;
+
+  for (char ch : networkAddress) {
+    int index = ch - '0';
+    if (current->children[index] == nullptr) {
+      break;
+    }
+    current = current->children[index];
+    if (current->routerNumber != -1) {
+      selectedRouter = current->routerNumber;
+    }
+  }
+  return selectedRouter;
+}
+
+void PrefixMatcher::insert(const std::string& address, int routerNumber) {
+  BinaryTrieNode* current = root;
+  for (char ch : address) {
+    int index = ch - '0';
+    if (current->children[index] == nullptr) {
+      current->children[index] = new BinaryTrieNode();
+    }
+    current = current->children[index];
+  }
+  current->routerNumber = routerNumber;
+}
