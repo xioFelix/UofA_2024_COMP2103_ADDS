565f169d309808c40625443a4ade23b153017956
Felix <a1878510@adelaide.edu.au>
2023-05-13 16:16:18 +0000

    2023年5月14日 01:46

---
+++ PrefixMatcher.h
@@ -0,0 +1,26 @@
+#ifndef PREFIXMATCHER_H
+#define PREFIXMATCHER_H
+
+#include <string>
+#include <vector>
+
+class BinaryTrieNode {
+ public:
+  BinaryTrieNode* children[2];
+  int routerNumber;
+
+  BinaryTrieNode();
+};
+
+class PrefixMatcher {
+ public:
+  PrefixMatcher();
+
+  int selectRouter(const std::string& networkAddress);
+  void insert(const std::string& address, int routerNumber);
+
+ private:
+  BinaryTrieNode* root;
+};
+
+#endif  // PREFIXMATCHER_H
